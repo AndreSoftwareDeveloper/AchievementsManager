8635f3670bce5cf0e1ebead656761e058e7693dd
AndreSoftwareDeveloper <105913538+AndreSoftwareDeveloper@users.noreply.github.com>
2023-06-04 12:40:53 -0400

    Update main.py

--- venv/main.py
+++ venv/main.py
@@ -3,11 +3,9 @@ from psnawp_api import PSNAWP
 import psnawp_api
 from enum import Enum
 
-#psnawp = PSNAWP('O0ovQDx5sZ4V5ERdET6pPmdS167GZNz6dbUKDtCdSGwgzKl492CZUU0eKjsxLQ6Z')
-
 try:
-    psnawp = input("Enter valid npsso code: ")       #będzie ogarnięty frontend to zrobić jakąś instrukcję dla użytkownika, odnośniki do odpowiednich stron
-    npssoSave = open("npssoSave.txt", 'w')
+    psnawp = input("Enter valid npsso code: ")       #TODO: będzie ogarnięty frontend to zrobić jakąś instrukcję dla użytkownika, odnośniki do odpowiednich stron
+    npssoSave = open("npssoSave.txt", 'w')           #TODO: tylko input w try, albo dać input w pętli wykonującej się dopóki użytkosniwk poda prawidłowy npsso
     print(psnawp, file=npssoSave)
     npssoSave.close()
 except psnawp_api.core.psnawp_exceptions.PSNAWPAuthenticationError:
@@ -22,7 +20,7 @@ print("Your nick is: " + client.online_id + "\n")
 choice = int(input("1 - Show my games\n" +
                    "2 -  Show trophies for specific game\n"))
 
-def sumTrophies(title, isEarned : bool):   
+def sumTrophies(title, isEarned : bool):                        #TODO:  don't use camel case, instead of this use: sum_trophies
     if isEarned:
          trophyStatus = title.earned_trophies
     else:
@@ -35,7 +33,7 @@ def sumTrophies(title, isEarned : bool):
 def showGamesData(x):
     match x:
         case 1:
-            for trophy_title in client.trophy_titles(limit = None):
+            for trophy_title in client.trophy_titles(limit = None):             #TODO: wywalić do do funkcji, w case ma być tylko 1 linia
                 print(trophy_title.title_name)
                 print(str(trophy_title.progress) + "%\t" + 
                       sumTrophies(trophy_title, True) +'/' + sumTrophies(trophy_title, False) + "\n")
