623d55d43265902ab66fc9d9001a510d3de8a550
Andre <andriej2301@gmail.com>
2023-08-08 02:08:00 +0200

    some steps ahead...

--- venv/main.py
+++ venv/main.py
@@ -2,6 +2,7 @@ import sys
 from psnawp_api import PSNAWP
 import psnawp_api
 from enum import Enum
+from psnawp_api.core import authenticator
 
 try:
     # psnawp = input("Enter valid npsso code: ")        #TODO: input powinien być w pętli wykonującej się dopóki użytkownik poda prawidłowy npsso
@@ -20,7 +21,7 @@ choice = int(input("1 - Show my games\n" +
                    "2 -  Show trophies for specific game\n"))
 
 
-def show_games_data(choice):
+def show_library_data(choice):
     match choice:
         case 1:
             show_my_games()
@@ -48,8 +49,35 @@ def sum_trophies(title, is_earned: bool):
                trophy_status.platinum)
 
 
-# for dupa in client.trophies():
-#      print(dupa)
-# dupa = psnawp.game_title
-#
-show_games_data(choice)
+def list_games():
+    data = []
+    titles = []
+    for trophy_title in client.trophy_titles(limit=None):
+        titles.append(trophy_title.title_name)
+        data.append(trophy_title)
+    return data, titles
+
+
+def trophies_for_game(title: str, data: list, titles: list):
+    if title in titles:
+        index = titles.index(title)
+        game = data[index]
+        get_trophy_summary_for_title()
+    else:
+        print("Game not found!")
+        exit()
+
+
+games_data = list_games()[0]  # TODO optimization
+games_titles = list_games()[1]
+show_library_data(choice)
+# trophies_for_game("Assassin's Creed® IV Black Flag", games_data, games_titles)
+
+authenticator = psnawp_api.core.authenticator.Authenticator(npsso)                  # TODO refactor
+requestBuilder = psnawp_api.utils.request_builder.RequestBuilder(authenticator)
+trophyTitles = psnawp_api.models.trophies.trophy_titles.TrophyTitles(requestBuilder, client.account_id)
+np_communication_id = trophyTitles.get_np_communication_id(requestBuilder, "CUSA13323_00", client.account_id)
+
+print(np_communication_id)
+trophy_titles_generator = client.trophy_summary()
+print(trophy_titles_generator)
