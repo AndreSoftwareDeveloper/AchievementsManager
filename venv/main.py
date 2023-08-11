70482915998b18891fe9de30c9a07277b3375b0c
Andre <andriej2301@gmail.com>
2023-08-12 00:51:55 +0200

    feature: Listing earned trophies for choosen game.

--- venv/main.py
+++ venv/main.py
@@ -1,8 +1,15 @@
 import sys
-from psnawp_api import PSNAWP
-import psnawp_api
 from enum import Enum
-from psnawp_api.core import authenticator
+
+import psnawp_api
+from psnawp_api import PSNAWP
+from psnawp_api.core.authenticator import Authenticator
+from psnawp_api.core.psnawp_exceptions import PSNAWPAuthenticationError
+from psnawp_api.models.game_title import GameTitle
+from psnawp_api.models.trophies.trophy import TrophyBuilder
+from psnawp_api.models.trophies.trophy_titles import TrophyTitles
+from psnawp_api.utils.request_builder import RequestBuilder
+from psnawp_api.models.search import Search
 
 try:
     # psnawp = input("Enter valid npsso code: ")        #TODO: input powinien być w pętli wykonującej się dopóki użytkownik poda prawidłowy npsso
@@ -11,14 +18,15 @@ try:
         npsso = npssoSave.readline()
     npssoSave.close()
     psnawp = PSNAWP(npsso)
-except psnawp_api.core.psnawp_exceptions.PSNAWPAuthenticationError as errorMessage:
+except PSNAWPAuthenticationError as errorMessage:
     print(errorMessage)
     sys.exit()
 
 client = psnawp.me()
 print("Your nick is: " + client.online_id + "\n")
 choice = int(input("1 - Show my games\n" +
-                   "2 -  Show trophies for specific game\n"))
+                   "2 - Show trophies for specific game\n" +
+                   "3 - Exit\n"))
 
 
 def show_library_data(choice):
@@ -26,9 +34,11 @@ def show_library_data(choice):
         case 1:
             show_my_games()
         case 2:
-            raise NotImplementedError("Option like that doesn't exist yet!")
+            trophies_for_game(request_builder, account_id)
+        case 3:
+            exit()
         case _:
-            print("There is no such option.")
+            print("There is no such option!")
 
 
 def show_my_games():
@@ -58,26 +68,22 @@ def list_games():
     return data, titles
 
 
-def trophies_for_game(title: str, data: list, titles: list):
-    if title in titles:
-        index = titles.index(title)
-        game = data[index]
-        get_trophy_summary_for_title()
-    else:
-        print("Game not found!")
-        exit()
+def trophies_for_game(requestBuilder, accountID):
+    title = input("Enter game title: ")
+    title_id = search.get_title_id(title)[1]
+    np_com_id = trophyTitles.get_np_communication_id(requestBuilder, title_id, accountID)
+    trophy_builder = TrophyBuilder(requestBuilder, np_com_id)
+    earned_trophies = trophy_builder.earned_game_trophies_with_metadata(accountID, "PS4", "default", 9999)
+    for trophy in earned_trophies:
+        if trophy.earned:
+            print(trophy)
 
 
-games_data = list_games()[0]  # TODO optimization
-games_titles = list_games()[1]
+authenticator = Authenticator(npsso)
+request_builder = RequestBuilder(authenticator)
+search = Search(request_builder)
+trophyTitles = TrophyTitles(request_builder, client.account_id)
+account_id = client.account_id
+games_data, games_titles = list_games()
+
 show_library_data(choice)
-# trophies_for_game("Assassin's Creed® IV Black Flag", games_data, games_titles)
-
-authenticator = psnawp_api.core.authenticator.Authenticator(npsso)                  # TODO refactor
-requestBuilder = psnawp_api.utils.request_builder.RequestBuilder(authenticator)
-trophyTitles = psnawp_api.models.trophies.trophy_titles.TrophyTitles(requestBuilder, client.account_id)
-np_communication_id = trophyTitles.get_np_communication_id(requestBuilder, "CUSA13323_00", client.account_id)
-
-print(np_communication_id)
-trophy_titles_generator = client.trophy_summary()
-print(trophy_titles_generator)
