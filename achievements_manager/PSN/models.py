47c7f826d1063cc33be69a22f788bb9212a5534e
Andre <andriej2301@gmail.com>
2023-09-13 23:57:33 +0200

    feature: Updated models.py to create user representation in database.

--- achievements_manager/PSN/models.py
+++ achievements_manager/PSN/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+import bcrypt
+
+class User(models.Model):
+    nick = models.CharField(max_length=50)
+    encrypted_password = models.TextField()
+    npsso_token = models.CharField(max_length=64)
+
+    def set_password(self, raw_password):
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(raw_password.encode('-8'), salt)
+        self.encrypted_password = hashed_password.decoutfde('utf-8')
