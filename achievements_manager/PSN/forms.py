fc7f20dc45e286a49b9bbedd3516815ede2df3df
Andre <andriej2301@gmail.com>
2023-10-07 23:23:12 +0200

    fix: refactor

--- achievements_manager/PSN/forms.py
+++ achievements_manager/PSN/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class PsnSignInForm(forms.Form):
     npsso = forms.CharField(
-        max_length = 64,
-        min_length = 64,
-        label = '',
-        widget = forms.TextInput ( attrs={'placeholder': 'Enter NPSSO token'} )
+        max_length=64,
+        min_length=64,
+        label='',
+        widget=forms.TextInput(attrs={'placeholder': 'Enter NPSSO token'})
     )
